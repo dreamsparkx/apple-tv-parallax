2aaa0067bb28b8bade22741189c8f57e8e3d7edb
Gaurav Bharti <dreamsparks@outlook.com>
2020-07-05 03:23:13 +0530

    added format

--- src/main/index.test.tsx
+++ src/main/index.test.tsx
@@ -3,19 +3,23 @@ import { render } from '@testing-library/react';
 import Parallax from './index';
 
 describe('Test Component', () => {
-    const renderComponent = () => render(<Parallax layers={[
-        'https://i.imgur.com/G2zsxeD.png',
-        'https://i.imgur.com/HZmIEdK.png',
-        'https://i.imgur.com/dp50zpz.png',
-        'https://i.imgur.com/DiWkFKd.png',
-        'https://i.imgur.com/LwojbKJ.png',
-    ]}
-                                                   isStatic={false}
-                                                   style={{
-                                                       width: '320px',
-                                                       height: '500px',
-                                                   }}
-    />);
+    const renderComponent = () =>
+        render(
+            <Parallax
+                layers={[
+                    'https://i.imgur.com/G2zsxeD.png',
+                    'https://i.imgur.com/HZmIEdK.png',
+                    'https://i.imgur.com/dp50zpz.png',
+                    'https://i.imgur.com/DiWkFKd.png',
+                    'https://i.imgur.com/LwojbKJ.png',
+                ]}
+                isStatic={false}
+                style={{
+                    width: '320px',
+                    height: '500px',
+                }}
+            />,
+        );
     it('check mounted', () => {
         const { getByTestId } = renderComponent();
         const rootComponent = getByTestId('root-div');
