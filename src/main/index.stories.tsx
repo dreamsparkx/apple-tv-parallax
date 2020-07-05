6f0066b6e432bb4b542af730611f9b810ce20025
Gaurav Bharti <dreamsparks@outlook.com>
2020-07-05 20:04:38 +0530

    added storybook deployer

--- src/main/index.stories.tsx
+++ src/main/index.stories.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions';
-import { withKnobs, text, select, array, object, boolean } from '@storybook/addon-knobs';
+import { withKnobs, array, object, boolean } from '@storybook/addon-knobs';
 import Parallax from './index';
 
 storiesOf('Parallax', module)
