968379a101b6619a2de093b0a16e4c8a17db7d2a
Gaurav Bharti <dreamsparks@outlook.com>
2020-07-07 00:47:31 +0530

    style(package.json): fixed prettier
    fixed prettier

--- rollup.config.js
+++ rollup.config.js
@@ -1,31 +1,25 @@
 import peerDepsExternal from 'rollup-plugin-peer-deps-external';
 import resolve from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
-import typescript from "rollup-plugin-typescript2";
+import typescript from 'rollup-plugin-typescript2';
 import image from '@rollup/plugin-image';
 
 const packageJson = require('./package.json');
 
 export default {
-    input: "src/index.ts",
+    input: 'src/index.ts',
     output: [
         {
             file: packageJson.main,
-            format: "cjs",
-            sourcemap: true
+            format: 'cjs',
+            sourcemap: true,
         },
         {
             file: packageJson.module,
-            format: "esm",
-            sourcemap: true
-        }
+            format: 'esm',
+            sourcemap: true,
+        },
     ],
-    external: ["styled-components"],
-    plugins: [
-        peerDepsExternal(),
-        resolve(),
-        commonjs(),
-        image(),
-        typescript({ useTsconfigDeclarationDir: true })
-    ]
+    external: ['styled-components'],
+    plugins: [peerDepsExternal(), resolve(), commonjs(), image(), typescript({ useTsconfigDeclarationDir: true })],
 };
